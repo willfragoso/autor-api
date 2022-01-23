d53d19058e1b3ac6253d97ee86e29964aec9d088
William Fragoso <will.fragoso@gmail.com>
2022-01-23 14:58:55 -0300

    Versionamento da estrutura inicial do projeto.

---
+++ src/main/java/com/examplo/autorapi/AutorApiApplication.java
@@ -0,0 +1,13 @@
+package com.examplo.autorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutorApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutorApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/examplo/autorapi/AutorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examplo.autorapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutorApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
